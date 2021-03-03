ce5c13ba45dbfc26580cb44a6fa85fe74eef1700
HCYETY <70583417+HCYETY@users.noreply.github.com>
2021-03-03 19:48:34 +0800

    生成随机数

--- JavaScript/initialization.js
+++ JavaScript/initialization.js
@@ -13,28 +13,41 @@ var game = {
         this.status = this.gameRunning;
         this.score = 0; // 游戏初始分数为0
         this.date = [];
-        for(var r=0; r<4; r++) {
-            this.date[r] = [];
-            for(var c=0; c<4; c++) {
-                this.date[r][c] = 0; // 将数组初始化
+        for(var row=0; row<4; row++) {
+            this.date[row] = [];
+            for(var list=0; list<4; list++) {
+                this.date[row][list] = 0; // 将数组初始化
             }
         }
     }
 }
+randomNumber();
+dateView();
 // a、生成随机数2、4
-
-for( ; ; ) {
-    var r = Math.floor(Math.random()*4); // 获取行的随机数，并取整
-    var c = Math.floor(Math.random()*4); // 获取列的随机数，并取整
-    if(this.date[r][c] == 0) {
-        var num = Math.random() > 0.5 ? 2 : 4; // 0.5说明产生随机数的概率为50%
-        this.date[r][c] = num; // 将随机产生的数值赋给data的随机位置上
-        break; // 退出循环
+function randomNumber() {
+    for( ; ; ) {
+        var row = Math.floor(Math.random()*4); // 获取行的随机数，并取整
+        var list = Math.floor(Math.random()*4); // 获取列的随机数，并取整
+        if(this.date[row][list] == 0) {
+            var num = Math.random() > 0.5 ? 2 : 4; // 0.5说明产生随机数的概率为50%
+            this.date[row][list] = num; // 将随机产生的数值赋给data的随机位置上
+            break; // 退出循环
+        }
     }
 }
 // b、在获取到随机数后我们要将随机数放到这些格子中去
-for(var r=0; r<4; r++) {
-    for(var c=0; c<4; c++) {
-
+function dateView() {
+    for(var row=0; row<4; row++) {
+        for(var list=0; list<4; list++) {
+            var div = document.getElementById("grid-cell-" + row + "-" + list);
+            // 获取数组里面的相对应数的位置的ID
+            if(this.date[row][list] == 0) {
+                div.innerHTML = "";
+                div.className = 'grid-cell';
+            } else {
+                div.innerHTML = this.date[row][list];
+                div.className = 'grid-cell n'+this.date[row][list];
+            }
+        }
     }
 }
