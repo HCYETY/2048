fc11d798281f25568127014913f2a87dc79e6d13
HCYETY <70583417+HCYETY@users.noreply.github.com>
2021-03-04 23:40:16 +0800

    生成数字方格

--- JavaScript/initialization.js
+++ JavaScript/initialization.js
@@ -9,44 +9,53 @@ var game = {
     gameOver:0, // 游戏结束
     gameRunning:1, // 游戏运行时
     status:1, // 添加游戏状态
-    start:function() {
-        this.status = this.gameRunning;
-        this.score = 0; // 游戏初始分数为0
-        this.date = [];
-        for(var row=0; row<4; row++) {
-            this.date[row] = [];
-            for(var list=0; list<4; list++) {
-                this.date[row][list] = 0; // 将数组初始化
-            }
+}
+start();
+randomNumber();
+dateView();
+function start() {
+    this.status = this.gameRunning;
+    this.score = 0; // 游戏初始分数为0
+    this.date = [];
+    for(var row=0; row<4; row++) {
+        this.date[row] = [];
+        for(var column=0; column<4; column++) {
+            this.date[row][column] = 0; // 将数组初始化
         }
     }
 }
-randomNumber();
-dateView();
 // a、生成随机数2、4
 function randomNumber() {
     for( ; ; ) {
-        var row = Math.floor(Math.random()*4); // 获取行的随机数，并取整
-        var list = Math.floor(Math.random()*4); // 获取列的随机数，并取整
-        if(this.date[row][list] == 0) {
+        row = Math.floor(Math.random()*4); // 获取行的随机数，并取整
+        column = Math.floor(Math.random()*4); // 获取列的随机数，并取整
+        if(this.date[row][column] == 0) {
+            break;
+        } else {
             var num = Math.random() > 0.5 ? 2 : 4; // 0.5说明产生随机数的概率为50%
-            this.date[row][list] = num; // 将随机产生的数值赋给data的随机位置上
+            this.date[row][column] = num; // 将随机产生的数值赋给data的随机位置上
             break; // 退出循环
         }
+        console.log(this.date[row][column]);
     }
 }
 // b、在获取到随机数后我们要将随机数放到这些格子中去
 function dateView() {
     for(var row=0; row<4; row++) {
-        for(var list=0; list<4; list++) {
-            var div = document.getElementById("grid-cell-" + row + "-" + list);
+        for(var column=0; column<4; column++) {
+            var div = document.getElementById("grid-cell-" + row + "-" + column);
+            console.log("grid-cell-" + row + "-" + column)
+            console.log(row)
+            console.log(column)
+            console.log(div)
             // 获取数组里面的相对应数的位置的ID
-            if(this.date[row][list] == 0) {
-                div.innerHTML = "";
+            if(this.date[row][column] == 0) {
+                div.innerText = "&nbsp;";
                 div.className = 'grid-cell';
+                
             } else {
-                div.innerHTML = this.date[row][list];
-                div.className = 'grid-cell n'+this.date[row][list];
+                div.innerText = this.date[row][column];
+                div.className = 'grid-cell n'+this.date[row][column];
             }
         }
     }
